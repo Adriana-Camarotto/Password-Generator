50048abae74e11f0e0e26c329b9f5abbc803094c
Adriana Camarotto <adriana.camarotto@gmail.com>
2023-02-14 17:37:33 +0000

    added alert to choose at least one type of character

--- script.js
+++ script.js
@@ -140,6 +140,8 @@ function getPasswordOptions() {
   }
   if (confirm("Do you want uppercase characters?")) {
     optionsArr = optionsArr.concat(upperCasedCharacters);
+  } else { 
+     alert("You must choose at least one type of character");
   }
   return true;  
 }
